7e30fbab6ca8190f8a14303f9a1b355ef29feddc
Rafael Romancini <rafael.romancini@gmail.com>
2017-08-31 22:47:59 -0300

    Adicionado layouts para os botões e fundos das mensagens e mais um pouco da parte de conversa

--- app/src/main/java/br/edu/ifspsaocarlos/sdm/ifspmessenger/ConversaActivity.java
+++ app/src/main/java/br/edu/ifspsaocarlos/sdm/ifspmessenger/ConversaActivity.java
@@ -2,14 +2,42 @@ package br.edu.ifspsaocarlos.sdm.ifspmessenger;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
 
-public class ConversaActivity extends AppCompatActivity {
+import java.util.List;
+
+public class ConversaActivity extends AppCompatActivity implements View.OnClickListener {
+    private int idContatoConversa;
+    private ArrayAdapter<String> mensagensAdapter;
+    private ListView mensagensListView;
+    private ImageButton mensagemButton;
+    private EditText mensagemEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conversa);
 
+        idContatoConversa = getIntent().getIntExtra("id_contato", 0);
+        mensagensAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        mensagensListView = (ListView)findViewById(R.id.msgListView);
+        mensagensListView.setAdapter(mensagensAdapter);
+        mensagemButton = (ImageButton)findViewById(R.id.sendMessageButton);
+        mensagemButton.setOnClickListener(this);
+        // rodar processo para ficar lendo banco de dados
+    }
 
+    @Override
+    public void onClick(View v) {
+        mensagemEditText = (EditText)findViewById(R.id.messageEditText);
+        String mensagem = mensagemEditText.getText().toString();
+        mensagemEditText.setText("");
+        // salvar mensagem no banco
+
+        //fazer push da mensagem para a API
     }
 }
